72d8d2ef14b48ed940e87a63c47359ad0667573e
Vladan <vladanmarkov00@gmail.com>
2023-07-20 15:19:56 +0200

    fixed a bug

--- src/embedFaceit.js
+++ src/embedFaceit.js
@@ -16,56 +16,69 @@ const {
 async function fStats(name){
 
     try{
-        var id = 0;
-
-        if(name === 'andrejicaker')
-            name  = "J0ker32"
-            
-        var player = await fr.nickStats(name)
-        id = player.player_id
-    }
-    catch(error){
-        console.log(error)
-    }
-
-    var temp = await fr.idStatsCheck(id)
-    var csgoStats = await fr.idStats(temp.player_id)
-    var regiont = await fr.nickStats(temp.nickname)
-    var country =  regiont.country
-    var skill_level = regiont.games.csgo["skill_level"].toString()
+        try{
+            var id = 0;
     
-    var skill_level_pic = "https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/level" + skill_level + ".png"
+            if(name === 'andrejicaker')
+                name  = "J0ker32"
+                
+            var player = await fr.nickStats(name)
+            id = player.player_id
+        }
+        catch(error){
+            console.log(error)
+        }
+    
+        var temp = await fr.idStatsCheck(id)
+        var csgoStats = await fr.idStats(temp.player_id)
+        var regiont = await fr.nickStats(temp.nickname)
+        var country =  regiont.country
+        var skill_level = regiont.games.csgo["skill_level"].toString()
+        
+        var skill_level_pic = "https://raw.githubusercontent.com/pvhil/FaceItDiscord/master/pictures/level" + skill_level + ".png"
+    
+        // console.log(temp)
+    
+        nick = temp.nickname
+        if(temp.nickname === 'J0ker32')
+            nick = 'Andrejica ker'
+    
+        const embed = new EmbedBuilder()
+                    .setColor('#FF5500')
+                    .setTitle('Stats for ' + nick)
+                    .addFields({
+                        name: "Country",
+                        value: countryCodeEmoji(country),
+                        inline: true
+                    },
+                    {
+                        name: "K/D",
+                        value : csgoStats.lifetime["Average K/D Ratio"].toString(),
+                        inline: true
+                    },
+    
+                    {
+                        name: "faceit elo",
+                        value: regiont.games.csgo["faceit_elo"].toString(),
+                        inline: true
+                    }
+                    )
+    
+                    .setAuthor({ name: 'Faceit level', iconURL: skill_level_pic})
+    
+        return embed
+    }
 
-    // console.log(temp)
+    catch(e){
+        const errembed = new EmbedBuilder()
+            .setColor("#ff0000")
+            .setTitle("Wrong faceit name :rage:")
+            .setDescription("This Faceit name is invalid! Try again")
+            .setTimestamp()
 
-    nick = temp.nickname
-    if(temp.nickname === 'J0ker32')
-        nick = 'Andrejica ker'
-
-    const embed = new EmbedBuilder()
-                .setColor('#FF5500')
-                .setTitle('Stats for ' + nick)
-                .addFields({
-                    name: "Country",
-                    value: countryCodeEmoji(country),
-                    inline: true
-                },
-                {
-                    name: "K/D",
-                    value : csgoStats.lifetime["Average K/D Ratio"].toString(),
-                    inline: true
-                },
-
-                {
-                    name: "faceit elo",
-                    value: regiont.games.csgo["faceit_elo"].toString(),
-                    inline: true
-                }
-                )
-
-                .setAuthor({ name: 'Faceit level', iconURL: skill_level_pic})
-
-    return embed
+        return errembed
+    }
+   
 }
 
 
